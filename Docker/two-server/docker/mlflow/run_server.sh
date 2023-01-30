3fef1db6a6430057f448ac0624b8fd3ddee0ff91
ctolon <cevat.batuhan.tolon@cern.ch>
2023-01-30 09:13:31 +0300

    postgre ssl disable

--- Docker/two-server/docker/mlflow/run_server.sh
+++ Docker/two-server/docker/mlflow/run_server.sh
@@ -1,5 +1,5 @@
 run() {
-  STORE_URI=postgresql+psycopg2://postgres:${POSTGRES_PASSWORD}@postgresql/mlflow
+  STORE_URI=postgresql+psycopg2://postgres:${POSTGRES_PASSWORD}@postgresql/mlflow?sslmode=disable
   echo "STORE_URI=$STORE_URI"
   echo "MLFLOW_ARTIFACT_URI=$MLFLOW_ARTIFACT_URI"
   mlflow server --host 0.0.0.0 --port 5000 --backend-store-uri $STORE_URI --default-artifact-root ftp://kral:123@127.0.0.1/mlartifacts --no-serve-artifacts
